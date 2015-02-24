332600fefa729909d7175b0d6e50489662608f5e
Bhavesh Gupta <BhaveshSGupta@gmail.com>
2015-02-24 18:27:32 +0530

    commit

---
+++ Class.EDcipher.php
@@ -0,0 +1,237 @@
+<?php
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Bhavesh Gupta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+class EDcipher{
+	
+		private $key;
+		private $method;
+
+		public function __construct()
+		{
+			$args = func_get_args();
+			$num = func_num_args();
+			print $num."<br>";
+			if(method_exists($this,$f = 'init_' . $num)) {
+				call_user_func_array(array($this,$f),$args);
+        }
+		}
+	
+	public function init_1($_key)
+	{
+    	$this->key = $_key;
+		$this->method="AES-256-CBC";
+	}
+ 
+    public function init_2($_key,$_method)
+	{
+    	$this->key = $_key;
+		$this->method = $_method;
+    }
+	
+	public function encrypt($text2Encrypt)
+	{
+		return openssl_encrypt($text2Encrypt, $this->method, $this->key);
+	}
+
+		public function decrypt($text2Decrypt){
+			return openssl_decrypt($text2Decrypt, $this->method, $this->key);
+		}
+	}
+
+/*
+List of all Supported Cipher Type
+AES-128-CBC
+AES-128-CFB
+AES-128-CFB1
+AES-128-CFB8
+AES-128-CTR
+AES-128-ECB
+AES-128-OFB
+AES-128-XTS
+AES-192-CBC
+AES-192-CFB
+AES-192-CFB1
+AES-192-CFB8
+AES-192-CTR
+AES-192-ECB
+AES-192-OFB
+AES-256-CBC
+AES-256-CFB
+AES-256-CFB1
+AES-256-CFB8
+AES-256-CTR
+AES-256-ECB
+AES-256-OFB
+AES-256-XTS
+BF-CBC
+BF-CFB
+BF-ECB
+BF-OFB
+CAMELLIA-128-CBC
+CAMELLIA-128-CFB
+CAMELLIA-128-CFB1
+CAMELLIA-128-CFB8
+CAMELLIA-128-ECB
+CAMELLIA-128-OFB
+CAMELLIA-192-CBC
+CAMELLIA-192-CFB
+CAMELLIA-192-CFB1
+CAMELLIA-192-CFB8
+CAMELLIA-192-ECB
+CAMELLIA-192-OFB
+CAMELLIA-256-CBC
+CAMELLIA-256-CFB
+CAMELLIA-256-CFB1
+CAMELLIA-256-CFB8
+CAMELLIA-256-ECB
+CAMELLIA-256-OFB
+CAST5-CBC
+CAST5-CFB
+CAST5-ECB
+CAST5-OFB
+DES-CBC
+DES-CFB
+DES-CFB1
+DES-CFB8
+DES-ECB
+DES-EDE
+DES-EDE-CBC
+DES-EDE-CFB
+DES-EDE-OFB
+DES-EDE3
+DES-EDE3-CBC
+DES-EDE3-CFB
+DES-EDE3-CFB1
+DES-EDE3-CFB8
+DES-EDE3-OFB
+DES-OFB
+DESX-CBC
+IDEA-CBC
+IDEA-CFB
+IDEA-ECB
+IDEA-OFB
+RC2-40-CBC
+RC2-64-CBC
+RC2-CBC
+RC2-CFB
+RC2-ECB
+RC2-OFB
+RC4
+RC4-40
+RC4-HMAC-MD5
+SEED-CBC
+SEED-CFB
+SEED-ECB
+SEED-OFB
+aes-128-cbc
+aes-128-cfb
+aes-128-cfb1
+aes-128-cfb8
+aes-128-ctr
+aes-128-ecb
+aes-128-gcm
+aes-128-ofb
+aes-128-xts
+aes-192-cbc
+aes-192-cfb
+aes-192-cfb1
+aes-192-cfb8
+aes-192-ctr
+aes-192-ecb
+aes-192-gcm
+aes-192-ofb
+aes-256-cbc
+aes-256-cfb
+aes-256-cfb1
+aes-256-cfb8
+aes-256-ctr
+aes-256-ecb
+aes-256-gcm
+aes-256-ofb
+aes-256-xts
+bf-cbc
+bf-cfb
+bf-ecb
+bf-ofb
+camellia-128-cbc
+camellia-128-cfb
+camellia-128-cfb1
+camellia-128-cfb8
+camellia-128-ecb
+camellia-128-ofb
+camellia-192-cbc
+camellia-192-cfb
+camellia-192-cfb1
+camellia-192-cfb8
+camellia-192-ecb
+camellia-192-ofb
+camellia-256-cbc
+camellia-256-cfb
+camellia-256-cfb1
+camellia-256-cfb8
+camellia-256-ecb
+camellia-256-ofb
+cast5-cbc
+cast5-cfb
+cast5-ecb
+cast5-ofb
+des-cbc
+des-cfb
+des-cfb1
+des-cfb8
+des-ecb
+des-ede
+des-ede-cbc
+des-ede-cfb
+des-ede-ofb
+des-ede3
+des-ede3-cbc
+des-ede3-cfb
+des-ede3-cfb1
+des-ede3-cfb8
+des-ede3-ofb
+des-ofb
+desx-cbc
+id-aes128-GCM
+id-aes192-GCM
+id-aes256-GCM
+idea-cbc
+idea-cfb
+idea-ecb
+idea-ofb
+rc2-40-cbc
+rc2-64-cbc
+rc2-cbc
+rc2-cfb
+rc2-ecb
+rc2-ofb
+rc4
+rc4-40
+rc4-hmac-md5
+seed-cbc
+seed-cfb
+seed-ecb
+seed-ofb
+*/
+?>
